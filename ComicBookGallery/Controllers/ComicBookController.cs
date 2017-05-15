48c8de73735cd8d5e5432ef561a5eafd9586a3c1
Kyle Jensen <kyle.w.jensen1@gmail.com>
2017-05-15 13:26:41 -0500

    Added comic book detail view

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,10 +8,8 @@ namespace ComicBookGallery.Controllers{
     public class ComicBookController : Controller {
 
         public ActionResult Detail() {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday) {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
+            return View();
         }
+
     }
 }
