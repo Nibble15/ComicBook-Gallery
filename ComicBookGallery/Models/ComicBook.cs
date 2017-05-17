7c559b7fe96aa7e6b6a2bd104b9a0907e86a22b9
Kyle Jensen <kyle.w.jensen1@gmail.com>
2017-05-17 13:54:04 -0500

    Updated Comic Book Model

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -12,5 +12,14 @@ namespace ComicBookGallery.Models {
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
+
+        //if problem with the following property return to "updating the data Model"
+        // on the treehouse mvc course and replace with his version of 
+        // the property which is a readonly - remember that it has a getter..
+        public string CoverImageFileName => $"{SeriesTitle.Replace(" ", "-").ToLower()} " +
+                      $"- {IssueNumber} .jpg";
+
     }
 }
